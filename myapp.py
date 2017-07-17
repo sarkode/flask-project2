8e28ff469f834a8ea622d8a2b267e6b5ac5e4186
Sarkodee <adjapongeric1@yahoo.com>
2017-07-17 14:52:09 +0000

    my project

---
+++ myapp.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def linux():
+        return render_template('linux.html')
+
+@app.route('/whereami')
+def whereami():
+        return "Kdua"
+
+if __name__ == '__main__':
+        app.run(host="0.0.0.0")
